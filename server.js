1d136b1b1b8f3e492458b87147ddbee1a812bc98
Kajal2202937 <bbhumi908@gmail.com>
2026-04-20 21:23:34 +0530

    final fix

--- server.js
+++ server.js
@@ -20,9 +20,20 @@ const insightRoutes = require("./routes/insightRoutes");
 const app = express();
 const PORT = process.env.PORT || 10000;
 
+const allowedOrigins = [
+  "https://erp-frontend-xi-seven.vercel.app",
+  "http://localhost:5173",
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
