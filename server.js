f820f6763fb7b4288454ac2daaee0c338115c4b4
Kajal2202937 <bbhumi908@gmail.com>
2026-04-24 20:31:34 +0530

    fix routing

--- server.js
+++ server.js
@@ -20,24 +20,15 @@ const insightRoutes = require("./routes/insightRoutes");
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-const allowedOrigins = [
-  "https://erp-frontend-xi-seven.vercel.app",
-  "http://localhost:5173",
-];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: [
+      "https://erp-frontend-xi-seven.vercel.app",
+      "http://localhost:5173",
+    ],
     credentials: true,
   }),
 );
-
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
